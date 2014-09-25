70bc2be82b606d8797f4d5f9b30bdcdb684eccca
Parker Moore <parkrmoore@gmail.com>
2014-09-24 22:58:46 -0700

    postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140925051158) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
