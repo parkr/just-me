b7bde8f5d74c963c8a5b2338c72d82572d367365
Parker Moore <parkrmoore@gmail.com>
2014-09-25 22:41:12 -0700

    Better UI for inserting posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,13 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
+    @post = Post.new
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-  end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
   end
 
   # GET /posts/1/edit
@@ -28,7 +24,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
