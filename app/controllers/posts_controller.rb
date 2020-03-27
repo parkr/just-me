7bcba67d8c5612d578c37b99a11f28e9e386e641
Parker Moore <parkrmoore@gmail.com>
2020-03-27 12:32:58 -0400

    Upgrade some things to make Rails 5.1 boot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
 
   # GET /posts
   # GET /posts.json
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ActiveRecord::Base
+class Post < ApplicationRecord
   belongs_to :user
   validates_presence_of :body
 end
